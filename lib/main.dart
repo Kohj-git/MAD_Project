040b26b5d41eae8b17e1d5568494d09c30bea392
zzozzo0427 <22000104@handong.ac.kr>
2024-05-23 17:23:51 +0900

    인사
    git push

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/game_over_screen.dart';
 
+// 하이루 여러분
 Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
