9c0d278718389282947ee9873d5669524c75d79e
zzozzo0427 <22000104@handong.ac.kr>
2024-05-24 20:04:18 +0900

    login, dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,77 @@
-import 'package:flame/flame.dart';
-import 'package:flame/game.dart';
-import 'package:flappy_bird_game/game/flappy_bird_game.dart';
-import 'package:flappy_bird_game/screens/main_menu_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'screens/game_over_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-// 하이루 여러분
-Future<void> main() async {
-  SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-  WidgetsFlutterBinding.ensureInitialized();
-  await Flame.device.fullScreen();
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    
+  ],
+);
 
-  final game = FlappyBirdGame();
-  runApp(
-    GameWidget(
-      game: game,
-      initialActiveOverlays: const [MainMenuScreen.id],
-      overlayBuilderMap: {
-        'mainMenu': (context, _) => MainMenuScreen(game: game),
-        'gameOver': (context, _) => GameOverScreen(game: game),
-      },
-    ),
-  );
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smoquit',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+      // TODO: Navigate to the next screen or perform necessary actions
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Smoquit',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: _handleSignIn,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.android),
+                  SizedBox(width: 8),
+                  Text('Sign in with Google'),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black, backgroundColor: Colors.white,
+                minimumSize: Size(double.infinity, 48),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
